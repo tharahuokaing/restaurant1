207ebfb628cc2fa2a1221e19a0eaf65846aa6060
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-21 18:00:39 +0700

    Change wedding audio source from music.mp3 to sgsa.mp3

--- music.js
+++ music.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "music.mp3";
+const weddingAudioSrc = "sgsa.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
